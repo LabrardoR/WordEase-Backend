d903dc6a053bcc16a6daa6672a0f6e268702805b
head <iamhead@163.com>
2024-09-13 22:12:10 +0800

    用户部分

---
+++ src/main/java/com/head/wordeasebackend/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.head.wordeasebackend.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.head.wordeasebackend.model.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户 Mapper
+ *
+ */
+
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
