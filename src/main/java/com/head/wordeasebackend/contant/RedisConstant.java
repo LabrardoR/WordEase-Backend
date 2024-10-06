542c36213a823f73914f2853f782df4814469628
head <iamhead@163.com>
2024-10-07 00:05:32 +0800

    Redis中的常量

---
+++ src/main/java/com/head/wordeasebackend/contant/RedisConstant.java
@@ -0,0 +1,12 @@
+package com.head.wordeasebackend.contant;
+
+/**
+ * 在 Redis 存储用到的常量
+ */
+public interface RedisConstant {
+
+    /**
+     * 用户注销常量
+     */
+    String USER_LOGOUT_KEY = "user:logout:";
+}
