62176851c908d06907a185c861613ed0a6d61374
head <iamhead@163.com>
2024-10-10 22:17:16 +0800

    更改全局异常处理器

--- src/main/java/com/head/wordeasebackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/head/wordeasebackend/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.head.wordeasebackend.exception;
 
 import com.head.wordeasebackend.common.BaseResponse;
 import com.head.wordeasebackend.common.ErrorCode;
+import com.head.wordeasebackend.common.Result;
 import com.head.wordeasebackend.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,15 +16,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BusinessException.class)
-    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
-        log.error("businessException: " + e.getMessage(), e);
-        return ResultUtils.error(e.getCode(), e.getMessage(), e.getDescription());
-    }
+//    @ExceptionHandler(BusinessException.class)
+//    public Result businessExceptionHandler(BusinessException e) {
+//        log.error("BusinessException: " + e.getMessage());
+//        return Result.fail();
+//    }
 
     @ExceptionHandler(RuntimeException.class)
-    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+    public Result runtimeExceptionHandler(RuntimeException e) {
         log.error("runtimeException", e);
-        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), "");
+        return Result.fail("运行时出现异常");
     }
 }
